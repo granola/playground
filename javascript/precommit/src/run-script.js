cd3ff128b21418b023a2145b7f4a62da9592a7b6
kaminora <kaminoura9207@gmail.com>
2018-09-09 14:51:09 +0900

    Get file path corresponding to script

--- javascript/precommit/src/run-script.js
+++ javascript/precommit/src/run-script.js
@@ -1,1 +1,21 @@
-console.log('read')
+const path = require('path')
+
+const [executor, ignoredBin, script, ...args] = process.argv
+
+if (script) {
+  spawnScript()
+} else {
+}
+
+function spawnScript() {
+  const relativeScriptPath = path.join(__dirname, './scripts', script)
+  const scriptPath = attemptResolve(relativeScriptPath)
+}
+
+function attemptResolve(...resolveArgs) {
+  try {
+    return require.resolve(...resolveArgs)
+  } catch (error) {
+    return null
+  }
+}
